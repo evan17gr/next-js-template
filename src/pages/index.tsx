7224e5b315b798488d7bcdae2ac1d79a7a85329b
ioanniskalvis <ioanniskalvis@hotmail.com>
2022-01-30 23:59:58 +0000

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Hello World!</h1>
+    </main>
+  );
+}
